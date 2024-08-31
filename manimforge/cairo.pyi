5f3c8186e1d85eb10895273b7cd9ac549c28a42a
JasonGrace2282 <aarush.deshpande@gmail.com>
2024-08-31 11:30:04 -0400

    Add docstring

--- manimforge/cairo.pyi
+++ manimforge/cairo.pyi
@@ -7,7 +7,15 @@ import cairo
 class CairoCamera:
     def __init__(self, *args, **kwargs) -> None: ...
 
-    def set_cairo_context_path(self, ctx: cairo.Context, vmobject: Any, points: npt.NDArray[np.float64]) -> None: ...
+    def set_cairo_context_path(self, ctx: cairo.Context, vmobject: Any, points: npt.NDArray[np.float64]) -> None:
+        """Traces a VMobject on the :class:`cairo.Context`.
+
+        Parameters
+        ----------
+            ctx: The pycairo context.
+            vmobject: A Manim Community VMobject
+            points: A numpy array of floats
+        """
 
     def __copy__(self) -> Self: ...
 
